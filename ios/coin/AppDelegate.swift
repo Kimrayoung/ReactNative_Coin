17b81b3f6895ad0356988ab5b3ea16154ef315a2
rayoung <rlafkdud122i@gmail.com>
2025-03-17 15:47:16 +0900

    인증번호 검색까지 완료, bottomSheet완료, locationView 선택에 따라 location바뀌도록 설정

--- ios/coin/AppDelegate.swift
+++ ios/coin/AppDelegate.swift
@@ -9,10 +9,11 @@ class AppDelegate: RCTAppDelegate {
     self.moduleName = "coin"
     self.dependencyProvider = RCTAppDependencyProvider()
 
+    // RNSplashScreen.show()
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
-
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
